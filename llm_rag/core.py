b9da5d47d78ff7b31c7b41d5dd3776af82ba5fea
Abhinandan <abhinandansendhav540@gmail.com>
2025-10-28 15:21:36 +0530

    fix requirements.txt.py

--- llm_rag/core.py
+++ llm_rag/core.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
